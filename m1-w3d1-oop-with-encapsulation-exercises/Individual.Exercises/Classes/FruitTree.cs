2d02a95d78b78890df814790940162abb5f32c54
Keet.Bailey <Keet.Bailey@gmail.com>
2018-06-05 11:57:32 -0400

    w3d1 update

--- m1-w3d1-oop-with-encapsulation-exercises/Individual.Exercises/Classes/FruitTree.cs
+++ m1-w3d1-oop-with-encapsulation-exercises/Individual.Exercises/Classes/FruitTree.cs
@@ -8,6 +8,30 @@ namespace Individual.Exercises.Classes
 {
     public class FruitTree
     {
-        
+        private string typeOfFruit;
+        private int piecesOfFruitLeft;
+
+        public int PiecesOfFruitLeft
+        {
+            get
+            {
+                return piecesOfFruitLeft;
+            }
+        }
+        public FruitTree(string typeOfFruit, int startingPiecesOfFruit)
+        {
+            this.piecesOfFruitLeft = startingPiecesOfFruit;
+            this.typeOfFruit = typeOfFruit;
+        }
+        public bool PickFruit(int numberOfPiecesToRemove)
+        {
+            if (piecesOfFruitLeft - numberOfPiecesToRemove >= 0)
+            {
+                piecesOfFruitLeft -= numberOfPiecesToRemove;
+                return true;
+            }
+
+            return false;
+        }
     }
 }
