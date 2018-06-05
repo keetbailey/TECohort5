859a51cb582607bb7353f8c29354b5aad4011265
Keet.Bailey <Keet.Bailey@gmail.com>
2018-06-04 21:42:23 -0400

    w3d1 update

--- m1-w3d1-oop-with-encapsulation-exercises/Individual.Exercises/Classes/Airplane.cs
+++ m1-w3d1-oop-with-encapsulation-exercises/Individual.Exercises/Classes/Airplane.cs
@@ -8,6 +8,68 @@ namespace Individual.Exercises.Classes
 {
     public class Airplane
     {
-       
+        private string planeNumber = "";
+        private int bookedFirstClassSeats = 0;
+        private int availableFirstClassSeats = 0;
+        private int totalFirstClassSeats = 0;
+        private int bookedCoachSeats = 0;
+        private int availableCoachSeats = 0;
+
+        public string PlaneNumber //Property 
+        {
+            get
+            {
+                return planeNumber;
+            }
+        }
+
+        public int BookedFirstClassSeats //Property 
+        {
+            get
+            {
+                return bookedFirstClassSeats;
+            }
+            private set
+            {
+                bookedFirstClassSeats = value;
+            }
+        }
+        public int AvailableFirstClassSeats //Property 
+        {
+            get
+            {
+                return availableFirstClassSeats;
+            }
+        }
+        public int TotalFirstClassSeats //Property 
+        {
+            get
+            {
+                return totalFirstClassSeats;
+            }
+        }
+        public int BookedCoachSeats //Property 
+        {
+            get
+            {
+                return bookedCoachSeats;
+            }
+            private set
+            {
+                bookedCoachSeats = value;
+            }
+        }
+        public int AvailableCoachSeats //Property 
+        {
+            get
+            {
+                return TotalFirstClassSeats - BookedFirstClassSeats;
+            }
+        }
+
+        public Airplane(string planeNumber, int totalFirstClassSeats, int totalCoachSeats) // constructor 
+        {
+
+        }
     }
 }
--- m1-w3d1-oop-with-encapsulation-exercises/Individual.Exercises/Classes/HomeworkAssignment.cs
+++ m1-w3d1-oop-with-encapsulation-exercises/Individual.Exercises/Classes/HomeworkAssignment.cs
@@ -8,8 +8,59 @@ namespace Individual.Exercises.Classes
 {
     public class HomeworkAssignment
     {
-        
+        private int totalMarks = 0;
+        private int possibleMarks = 0;
+        private string submitterName = "";
+        private string letterGrade = "";
 
+        public int TotalMarks //Property 
+        {
+            get
+            {
+                return totalMarks;
+            }
+            set
+            {
+                totalMarks = value;
+            }
+        }
+        public int PossibleMarks //property
+        {
+            get
+            {
+                return possibleMarks;
+            }
+            
+        }
+        public string SubmitterName //property - no set due to ReadOnly
+        {
+            get
+            {
+                return submitterName;
+            }
+        }
+        public string LetterGrade  //property
+        {
+            get
+            {
+                double scorePercent = (double)totalMarks / possibleMarks; //assign double from casted integer values to utilize in "If/Else" statement
+                if (scorePercent >= 0.90)
+                    return "A";
+                else if (scorePercent >= 0.80)
+                    return "B";
+                else if (scorePercent >= 0.70)
+                    return "C";
+                else if (scorePercent >= 0.60)
+                    return "D";
+                else
+                    return "F";
+            }
+           
+        }
 
+        public HomeworkAssignment(int possibleMarks, string submitterName) //Constructor - no type associated
+        {
+            this.possibleMarks = possibleMarks;
+        }
     }
 }
