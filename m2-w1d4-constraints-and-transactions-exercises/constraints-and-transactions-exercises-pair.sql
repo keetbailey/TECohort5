561c2252c450e2720ee9986a4231bd34bfd348ba
Keet.Bailey <Keet.Bailey@gmail.com>
2018-06-22 10:55:07 -0400

    Constraints and Transactions update

--- m2-w1d4-constraints-and-transactions-exercises/constraints-and-transactions-exercises-pair.sql
+++ m2-w1d4-constraints-and-transactions-exercises/constraints-and-transactions-exercises-pair.sql
@@ -4,31 +4,92 @@
 -- 1. Add Superman's hometown, Smallville, Kansas to the city table. The 
 -- countrycode is 'USA', and population of 45001. (Yes, I looked it up on 
 -- Wikipedia.)
+SELECT * FROM city WHERE district = 'Kansas';
+
+INSERT INTO city(id, name, countrycode, district, population)
+VALUES (5000, 'Smallville', 'USA', 'Kansas', 45001);
 
 -- 2. Add Kryptonese to the countrylanguage table. Kryptonese is spoken by 0.0001
 -- percentage of the 'USA' population.
+SELECT * FROM countrylanguage WHERE language is not null
+ORDER BY countrycode;
+
+INSERT INTO countrylanguage(countrycode, language, isofficial, percentage)
+VALUES('USA', 'Kryptonese', 0, 0.0001);
 
 -- 3. After heated debate, "Kryptonese" was renamed to "Krypto-babble", change 
 -- the appropriate record accordingly.
+SELECT * FROM countrylanguage WHERE language is not null
+ORDER BY countrycode;
+
+
+UPDATE countrylanguage
+SET language = 'Krypto-babble'
+WHERE language = 'Kryptonese';
 
 -- 4. Set the US captial to Smallville, Kansas in the country table.
+SELECT * FROM country WHERE code = 'USA'
+UPDATE country
+SET capital = 5000
+WHERE code = 'USA';
 
 -- 5. Delete Smallville, Kansas from the city table. (Did it succeed? Why?)
+SELECT * FROM city WHERE district = 'Kansas'
+DELETE FROM city
+WHERE id = 5000;
+
+Succeeded
 
 -- 6. Return the US captial to Washington.
+SELECT * FROM city WHERE countrycode= 'USA'
+SELECT * FROM country WHERE code = 'USA'
+
+
+UPDATE country
+SET capital = 3813
+WHERE code = 'USA';
 
 -- 7. Delete Smallville, Kansas from the city table. (Did it succeed? Why?)
+
+DELETE FROM city
+WHERE id = 5000;
+
+-- Did not succeed due to previous deletion - no errors, just 0 rows affected 
 
 -- 8. Reverse the "is the official language" setting for all languages where the
 -- country's year of independence is within the range of 1800 and 1972 
 -- (exclusive). 
 -- (590 rows affected)
 
+UPDATE countrylanguage
+SET isofficial = NOT isofficial
+WHERE countrycode IN 
+(
+	SELECT code 
+	FROM country
+	WHERE indepyear BETWEEN 1801 AND 1971
+);
+
+--- Need to revisit - KSB
+
 -- 9. Convert population so it is expressed in 1,000s for all cities. (Round to
 -- the nearest integer value greater than 0.)
--- (4079 rows affected)
+-- (4068 rows affected)
+SELECT * FROM city WHERE countrycode = 'USA';
+UPDATE city
+SET population = ceiling(population / 1000)
+WHERE population > 0;
 
 -- 10. Assuming a country's surfacearea is expressed in miles, convert it to 
 -- meters for all countries where French is spoken by more than 20% of the 
 -- population.
 -- (7 rows affected)
+
+UPDATE country
+SET surfacearea = surfacearea * 1609.344
+WHERE code IN 
+(
+	SELECT countrycode
+	FROM countrylanguage
+	WHERE language = 'French' AND percentage >= 20
+);
