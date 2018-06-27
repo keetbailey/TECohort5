4f53d37bb90d667d167ecc96afcbd6e0bcb3c941
Keet.Bailey <Keet.Bailey@gmail.com>
2018-06-26 20:11:55 -0400

    DALs created

--- m2-w2d1-dao-exercises/ProjectDB/DAL/DepartmentSqlDAL.cs
+++ m2-w2d1-dao-exercises/ProjectDB/DAL/DepartmentSqlDAL.cs
@@ -4,12 +4,16 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Data.SqlClient;
 
 namespace ProjectDB.DAL
 {
     public class DepartmentSqlDAL
     {
         private string connectionString;
+        private const string SQL_SelectDepartments = "SELECT * FROM department;";
+        private const string SQL_UpdateDepartment = "UPDATE department SET name = @name WHERE department_id = @id;";
+        private const string SQL_NewDepartment = "INSERT INTO department VALUES (@name);";
 
         // Single Parameter Constructor
         public DepartmentSqlDAL(string dbConnectionString)
@@ -19,18 +23,85 @@ namespace ProjectDB.DAL
 
         public List<Department> GetDepartments()
         {
-            throw new NotImplementedException();
+            List<Department> output = new List<Department>();
+
+            try
+            {
+                using (SqlConnection conn = new SqlConnection(connectionString))
+                {
+                    conn.Open();
+
+                    SqlCommand cmd = new SqlCommand(SQL_SelectDepartments, conn);
+                    SqlDataReader reader = cmd.ExecuteReader();
+
+                    while (reader.Read())
+                    {
+                        Department d = GetDepartmentReader(reader);
+
+                        output.Add(d);
+                    }
+                }
+            }
+            catch (SqlException ex)
+            {
+                throw;
+            }
+
+            return output;
+        }
+
+        private Department GetDepartmentReader(SqlDataReader reader)
+        {
+            Department d = new Department();
+            d.Id = Convert.ToInt32(reader["department_id"]);
+            d.Name = Convert.ToString(reader["name"]);
+
+            return d;
         }
 
         public bool CreateDepartment(Department newDepartment)
         {
-            throw new NotImplementedException();
+            try
+            {
+                using (SqlConnection conn = new SqlConnection(connectionString))
+                {
+                    conn.Open();
+
+                    SqlCommand cmd = new SqlCommand(SQL_NewDepartment, conn);
+                    cmd.Parameters.AddWithValue("@name", newDepartment.Name);
+
+                    int rowsAffected = cmd.ExecuteNonQuery();
+
+                    return (rowsAffected > 0);
+                }
+            }
+            catch (SqlException ex)
+            {
+                throw;
+            }
         }
 
         public bool UpdateDepartment(Department updatedDepartment)
         {
-            throw new NotImplementedException();
-        }
+            try
+            {
+                using (SqlConnection conn = new SqlConnection(connectionString))
+                {
+                    conn.Open();
 
+                    SqlCommand cmd = new SqlCommand(SQL_UpdateDepartment, conn);
+                    cmd.Parameters.AddWithValue("@name", updatedDepartment.Name);
+                    cmd.Parameters.AddWithValue("@id", updatedDepartment.Id);
+
+                    int rowsAffected = cmd.ExecuteNonQuery();
+
+                    return (rowsAffected > 0);
+                }
+            }
+            catch (SqlException ex)
+            {
+                throw;
+            }
+        }
     }
 }
--- m2-w2d1-dao-exercises/ProjectDB/DAL/EmployeeSqlDAL.cs
+++ m2-w2d1-dao-exercises/ProjectDB/DAL/EmployeeSqlDAL.cs
@@ -10,6 +10,9 @@ namespace ProjectDB.DAL
     public class EmployeeSqlDAL
     {
         private string connectionString;
+        private const string SQL_SelectAllEmployees = "SELECT * FROM employee;";
+        private const string SQL_SearchEmployees = "SELECT * FROM employee WHERE first_name LIKE @firstname AND last_name LIKE @lastname;";
+        private const string SQL_FreeEmployees = "SELECT * FROM employee WHERE employee_id NOT IN (SELECT employee_id FROM project_employee);";
 
         // Single Parameter Constructor
         public EmployeeSqlDAL(string dbConnectionString)
